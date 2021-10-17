e8806385e8d60b62b23590b1fdcaf986a67d33af
Kazakova Natalia Vladimirovna <natkazakova@ozon.ru>
2021-10-17 11:58:30 +0300

    Initial Commit

---
+++ InstagrammClone/InstagrammCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagrammCloneApp.swift
+//  InstagrammClone
+//
+//  Created by Kazakova Nataliya on 17.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagrammCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
